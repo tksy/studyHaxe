47a5ff52445fc7b3a1840e1eca84de1fddf38729
tksy <github@tsukasaya.info>
2014-10-26 22:03:54 +0900

    no message

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+(function () { "use strict";
+var HelloWorld = function() { };
+HelloWorld.main = function() {
+	console.log("Hello World !!");
+};
+HelloWorld.main();
+})();
+
+//# sourceMappingURL=HelloWorld.js.map
